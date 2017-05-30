57da48a15f04e0e6bce92c0c7ab58f34cf5cbfef
lnoel <lnoel@uchicago.edu>
2017-05-29 22:33:16 -0500

    configured to run daily

--- NHTest.py
+++ NHTest.py
@@ -1,13 +1,17 @@
 import subprocess
 from news_api import *
 import os.path
+import schedule
+import pandas as pd
+from time import strftime
+import time
 
 def process_headlines():
     process = subprocess.Popen(['java', '-jar', 'NHTextProcessor.jar'],
     	stdout=subprocess.PIPE)
     rating = process.stdout.read().decode('utf-8').split()
 
-    return rating
+    return rating[0]
 
 def update_input():
     api_key = read_api_key()
@@ -23,8 +27,40 @@ def write_textfile(text,title,path):
     for row in text:
         if row:
             file.write(row) 
-    file.close() 
+    file.close()
+
+def read_nh_datafile(verbose=False):
+    if os.path.isfile('nh_data.csv'):
+        df = pd.read_csv('nh_data.csv', encoding="utf-8")
+    else:
+        cols = ["rating", "date", "id"]
+        df = pd.DataFrame(columns=cols)
+    df.set_index('id')
+    return df
+
+def write_nh_datafile(df):
+    pd.DataFrame.to_csv(df, 'nh_data.csv', index=False,encoding="utf-8")
+
+def job():
+    try:
+        update_input()
+        rating = process_headlines()
+    except:
+        rating = 0
+    df = read_nh_datafile()
+    id_ = len(df)
+    temp_data = pd.DataFrame({'id':id_, 'rating':rating, 
+        'date':time.strftime("%c")}, index=['id'])
+    df = pd.concat([df, temp_data])
+    write_nh_datafile(df)
 
 if __name__ == '__main__':
-    update_input()
-    rating = process_headlines()
+    job()
+    schedule.every().monday.at("08:28").do(job)
+    schedule.every().tuesday.at("08:28").do(job)
+    schedule.every().wednesday.at("08:28").do(job)
+    schedule.every().thursday.at("08:28").do(job)
+    schedule.every().friday.at("08:28").do(job)
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
