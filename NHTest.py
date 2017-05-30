4b56dc1476f729d20e09ecaec5fde981715bb9a7
lnoel <lnoel@uchicago.edu>
2017-05-29 22:01:11 -0500

    made work with news_api

--- NHTest.py
+++ NHTest.py
@@ -1,8 +1,30 @@
 import subprocess
+from news_api import *
+import os.path
 
+def process_headlines():
+    process = subprocess.Popen(['java', '-jar', 'NHTextProcessor.jar'],
+    	stdout=subprocess.PIPE)
+    rating = process.stdout.read().decode('utf-8').split()
 
-process = subprocess.Popen(['java', '-jar', 'NHTextProcessor.jar'],
-	stdout=subprocess.PIPE)
-rating = process.stdout.read().decode('utf-8').split()
+    return rating
 
-print(rating)
+def update_input():
+    api_key = read_api_key()
+    headlines = get_all_descs(api_key)
+
+    write_textfile(headlines, 'input.txt','NHResources')
+
+def write_textfile(text,title,path):    
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file_name = path + '/'+ title
+    file = open(file_name,'w') 
+    for row in text:
+        if row:
+            file.write(row) 
+    file.close() 
+
+if __name__ == '__main__':
+    update_input()
+    rating = process_headlines()
