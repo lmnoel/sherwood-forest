3bab071f919dff072dfdb550901ff4afd65f740c
Justin Nichols <justinni@usc.edu>
2017-06-15 20:42:12 -0700

    Reverted

--- BasicNHTest.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-process = subprocess.Popen(['java', '-jar', 'NHTextProcessor.jar'],
-    	stdout=subprocess.PIPE)
-rating = process.stdout.read().decode('utf-8').split()
-
-print (rating)
