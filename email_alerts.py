f9e648b2d707268e4e6b2222b12021a6a2b05958
lnoel <lnoel@uchicago.edu>
2017-09-23 12:31:44 -0500

    send notifications

--- email_alerts.py
+++ email_alerts.py
@@ -1,13 +1,24 @@
 #Logan Noel
-#
 #March 31 2017
 
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def send_mail(toaddr,subject,message):
-    fromaddr = "quickalert.py@gmail.com"
+def send_mail(fromaddr, toaddr, subject, message, pswd):
+    '''
+    Easily send a notification to yourself or as many
+    people as you want.
+
+        fromaddr: string (valid gmail address)
+        pswd: string - login to fromaddr
+        toaddr: list of strings (valid email addresses)
+        subject: string
+        message: string
+
+    Returns True if message sent, else False.
+    '''
+
     base_message = '''
     \n\n\n
     ==================================================
@@ -15,22 +26,21 @@ def send_mail(toaddr,subject,message):
     ==================================================
     '''
     message = message + base_message
-    msg = MIMEMultipart()
-    msg['From'] = "quickalert.py@gmail.com"
-    msg['To'] = toaddr
-    msg['Subject'] = subject
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = fromaddr
+        msg['To'] = toaddr
+        msg['Subject'] = subject
 
-    body = message
-    msg.attach(MIMEText(body, 'plain'))
+        body = message
+        msg.attach(MIMEText(body, 'plain'))
 
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(fromaddr, "mO9N3GYVT")
-    text = msg.as_string()
-    server.sendmail(fromaddr, toaddr, text)
-    server.quit()
-
-def wide_alert(subject, message):
-    toaddrs = ['noel.logan2@gmail.com', 'justinni@usc.edu']
-    for addr in toaddrs:
-        send_mail(addr, subject, message)
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.starttls()
+        server.login(fromaddr, pswd)
+        text = msg.as_string()
+        server.sendmail(fromaddr, toaddr, text)
+        server.quit()
+        return True
+    except:
+        return False
