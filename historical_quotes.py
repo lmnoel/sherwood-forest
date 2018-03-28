19d65a14a4fd0e1d7f39f242e9d703c0ae5ee36a
lnoel <lnoel@uchicago.edu>
2018-03-27 22:46:07 -0500

    made it actually write to csv

--- historical_quotes.py
+++ historical_quotes.py
@@ -1,6 +1,9 @@
 import requests
+import csv
+import os
 
 API_KEY = 'KRFXDF9PUE9BNKJ6'
+tickers = ['SPY', 'GLD'] #if you want to downlaod multiple files at once
 
 def download_data(ticker, begin_date='2018-03-23', end_date='2015-03-23', output_type='csv'):
     res = requests.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={}&apikey={}&datatype={}&outputsize=full'.format(ticker, API_KEY, output_type))
@@ -9,3 +12,19 @@ def download_data(ticker, begin_date='2018-03-23', end_date='2015-03-23', output
     else:
         return res.text.split('\r\n')
     return res
+
+
+def run():
+
+    if not os.path.exists('quotes/'):
+        os.mkdir('quotes')
+    for ticker in tickers:
+        res = download_data(ticker=ticker)
+        filename = 'quotes/{}_data.csv'.format(ticker)
+        with open(filename, 'w') as csvwriter:
+            writer = csv.writer(csvwriter)
+            for line in res:
+                writer.writerow(line.split(','))
+
+if __name__ == '__main__':
+    run()
